235bc3cb3f8d951a97d76f74de0f5343384559bf
admin <admin@DESKTOP-FKR8P39>
2025-03-18 15:14:55 +0530

    changed in testcase

--- test123/src/test/java/Pac2/OrgTest.java
+++ test123/src/test/java/Pac2/OrgTest.java
@@ -4,11 +4,20 @@ import org.testng.annotations.Test;
 
 public class OrgTest {
 	@Test
-	public void createorgTest() {
+	public void createorgTest() {String URL = System.getProperty("url");
+	String BROWSER = System.getProperty("browser","chrome");
+	String UN = System.getProperty("username");
+	String PW = System.getProperty("password");
+	
+	System.out.println(URL);
+	System.out.println(BROWSER);
+	System.out.println(UN);
+	System.out.println(PW);
 		
 		System.out.println("EXECUTE createorgTest");
 
 	}
+	@Test
 	public void modifyorgTest() {
 		
 		
